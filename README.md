ba14a5dfdab69bbd127b445ddb86e7b6c94af179
Chris Fusting <cfusting@gmail.com>
2017-11-30 11:44:21 -0500

    Deleted broken tests, updated readme and setup

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
 # Fast Genetic Programming
 fastgp is a numpy implementation of [genetic programming](https://en.wikipedia.org/wiki/Genetic_programming) built on top of [deap](https://github.com/DEAP/deap). It is the core library for [fastsr](https://github.com/cfusting/fast-symbolic-regression), a symbolic regression package for Python.
+It's primary contribution is an implementation of AFPO<a href="#lc-1">\[1\]</a> which is compatible with any deap toolbox.
 
 fastgp was designed and developed by the [Morphology, Evolution & Cognition Laboratory](http://www.meclab.org/) at the University of Vermont. It extends research code which can be found [here](https://github.com/mszubert/gecco_2016).
+
+Installing
+----------
+fastgp is compatible with Python 2.7+.
+```bash
+pip install fastgp
+```
+
+Example Usage
+-------------
+fastgp is a core library and as such there are no examples in this repository.
+Check out [fastsr](https://github.com/cfusting/fast-symbolic-regression) for an example of fastgp's use in Symbolic Regression.
+
+Literature Cited
+----------------
+1. Michael Schmidt and Hod Lipson. 2011. Age-fitness pareto optimization. In Genetic Programming Theory and Practice VIII. Springer, 129–146.<a name="lc-2"></a>
--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
-name = 'fastgp'
-version = '0.1.0'
-description = 'Blazing fast genetic programming.'
-author = 'Marcin Szubert'
-author_email = 'marcin.g.szubert@gmail.com'
-license = 'GNU GPLv3'
-classifiers = [
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Science/Research',
-    'Topic :: Scientific/Engineering :: Artificial Intelligence',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Programming Language :: Python :: 3'
-]
-keywords = 'evolution machine learning artificial intelligence'
-install_requires = [
-    'sklearn',
-    'numpy',
-    'gp',
-    'deap'
-]
-python_requires = '>=3'
+from setuptools import setup, find_packages
+
+setup(
+    name='fastgp',
+    version='0.1.0',
+    description='Fast genetic programming.',
+    author='Chris Fusting',
+    author_email='cfusting@gmail.com',
+    license='GNU GPLv3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='evolution machine learning artificial intelligence',
+    install_requires=[
+        'scipy',
+        'deap',
+        'cachetools',
+        'numpy',
+    ],
+    python_requires='>=2.7',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    url='https://github.com/cfusting/fastgp'
+)
